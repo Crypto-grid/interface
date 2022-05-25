fa10dcafa2f99c2da79efb3009854e9b478a0de2
Ian Shaw <ianshaw93@gmail.com>
2022-05-25 23:40:10 +0100

    bootstrap css added

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { Web3ReactProvider } from '@web3-react/core'
-import './index.css'
+import { Web3ReactProvider } from '@web3-react/core';
+import './App.css';
+import './index.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { connectors, Web3Provider } from './modules/provider/Provider'
+
+document.body.style.overflow = "hidden"; //removes scroll from parent
+document.body.classList.add("no-scroll");
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
