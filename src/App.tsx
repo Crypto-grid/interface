b4b6d53ae0329803a24b80ecf0f5f18f557b0fc0
Ian Shaw <ianshaw93@gmail.com>
2022-05-25 23:30:19 +0100

    removed scroll error - however bootstrap etc components not showing in repo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,19 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import ThreeComponent from './modules/game'
 import { Navbar } from './modules/navbar/Navbar'
+import { ThemeSwitcherProvider } from 'react-css-theme-switcher';
+
+import './modules/styles/css/tailwind-base.pcss';
+import './modules/styles/css/tailwind-components.pcss';
+import './modules/styles/css/tailwind-utilities.pcss';
+import './modules/styles/css/app.css';
+
+// setup themes for theme switcher
+const themes = {
+  dark: './dark-theme.css',
+  light: './light-theme.css',
+};
+
 
 function App() {
   const [connected, setConnected] = useState(true)
@@ -18,6 +31,7 @@ function App() {
     <div className="App">
       <Navbar/>
       {connected ? <ThreeComponent /> : <div>Loading...</div>}
+      {/* <ThemeSwitcherProvider themes={themes}/> */}
     </div>
   )
 }
