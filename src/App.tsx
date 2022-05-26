9143a2ea9b671bc3b8d7071b6699b7261b772322
Ian Shaw <ianshaw93@gmail.com>
2022-05-26 09:09:03 +0100

    getContractInfo added

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import './App.css'
 import ThreeComponent from './modules/game'
 import { Navbar } from './modules/navbar/Navbar'
 import { ThemeSwitcherProvider } from 'react-css-theme-switcher';
+import { ethers } from "ethers"
 
 // import './modules/styles/css/tailwind-base.pcss';
 // import './modules/styles/css/tailwind-components.pcss';
@@ -19,9 +20,39 @@ import { ThemeSwitcherProvider } from 'react-css-theme-switcher';
 // };
 
 
-function App() {
+async function App() {
   const [connected, setConnected] = useState(true)
   const web3 = useWeb3React()
+
+  const hardhatTestnetUrl = 'http://localhost:8545';
+  const networkUrl = hardhatTestnetUrl;
+  const provider = new ethers.providers.JsonRpcProvider(networkUrl);
+
+  // const getBalance = () => {
+  const balance = await provider.getBalance(address); //wallet address
+  const balanceFormatted = ethers.utils.formatEther(balance);
+
+  const ERC20_ABI = [//can have more rows if required
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+    "function totalSupply() view returns (uint256)",
+    "function balanceOf(address) view returns (uint)",
+  ]
+  // }
+  async function getContract(address, ERC20_ABI, provider) {
+    const contract = await new ethers.Contract(address, ERC20_ABI, provider)
+    return contract
+  } 
+
+
+  async function getContractInfo() {
+    const name = await contract.name();
+    const symbol = await contract.symbol();
+    const totalSupply = await contract.totalSupply();
+    const balance = await balanceOf(address);
+    const balanceFormatted = ethers.utils.formatEther(balance);
+  } 
+
 
   useEffect(() => {
       const activate = web3.connector.activate() as Promise<void>
